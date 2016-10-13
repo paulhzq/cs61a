a8ca6fca3819698294b9ea9526f7f2a293b5c8a5
Paul Huang <paul_hzq@hotmail.com>
2016-10-12 21:16:45 -0400

    add vitamin file for hw3

---
+++ homework/hw03/vitamin/vitamin03.py
@@ -0,0 +1,62 @@
+HW_SOURCE_FILE = 'vitamin03.py'
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    assert type(k)==int 
+    "*** YOUR CODE HERE ***"
+    """ iteration
+    while k>0:
+        digit,k = k%10,k//10
+        if digit ==7:
+            return True
+    return False
+    """ 
+
+    #recursion
+    if k%10==7:
+        return True
+    elif k<10:
+        return False
+    else:
+        return has_seven(k//10)
+
+
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return term(1)
+    else:
+        return term(n)+summation(n-1,term)
+
