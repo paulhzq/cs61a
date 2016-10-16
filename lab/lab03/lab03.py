3de6323379f634ae14772aafa9be23c6bc1386b2
Paul Huang <paul_hzq@hotmail.com>
2016-10-15 21:12:05 -0500

    finish the basic lab03

--- lab/lab03/lab03.py
+++ lab/lab03/lab03.py
@@ -12,6 +12,17 @@ def gcd(a, b):
     40
     """
     "*** YOUR CODE HERE ***"
+    if a>=b:
+        if a%b==0:
+            return b
+        else:
+            return gcd(b,a%b)
+    else:
+        if b%a==0:
+            return a
+        else:
+            return gcd(b%a,a)
+
 
 def hailstone(n):
     """Print out the hailstone sequence starting at n, and return the
@@ -29,3 +40,14 @@ def hailstone(n):
     7
     """
     "*** YOUR CODE HERE ***"
+    k=0
+    while n!=1:
+        print(n)
+        if n%2==0:
+            n=n//2
+        else:
+            n=3*n-1
+        k+=1
+    print(n)
+    return k+1
+
