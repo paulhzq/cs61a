98e5753e303b6e2e8b98f8167929a311182a871e
Paul Huang <paul_hzq@hotmail.com>
2016-11-11 23:48:51 -0600

    add lab07

---
+++ lab/lab07/lab07.py
@@ -0,0 +1,180 @@
+## Recursive Objects ##
+
+# Q2
+def list_to_link(lst):
+    """Takes a Python list and returns a Link with the same elements.
+
+    >>> link = list_to_link([1, 2, 3])
+    >>> print_link(link)
+    <1 2 3>
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q3
+def link_to_list(link):
+    """Takes a Link and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> link_to_list(link)
+    [1, 2, 3, 4]
+    >>> link_to_list(Link.empty)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q4
+def remove_all(link , value):
+    """Remove all the nodes containing value. Assume there exists some
+    nodes to be removed and the first element is never removed.
+
+    >>> l1 = Link(0, Link(2, Link(2, Link(3, Link(1, Link(2, Link(3)))))))
+    >>> print_link(l1)
+    <0 2 2 3 1 2 3>
+    >>> remove_all(l1, 2)
+    >>> print_link(l1)
+    <0 3 1 3>
+    >>> remove_all(l1, 3)
+    >>> print_link(l1)
+    <0 1>
+    """
+    "*** YOUR CODE HERE ***"
+
+# Linked List Class
+class Link:
+    """A linked list.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.first
+    1
+    >>> s.rest
+    Link(2, Link(3))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is Link.empty:
+            return 'Link({})'.format(self.first)
+        else:
+            return 'Link({}, {})'.format(self.first, repr(self.rest))
+
+    def __len__(self):
+        """ Return the number of items in the linked list.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> len(s)
+        3
+        >>> s = Link.empty
+        >>> len(s)
+        0
+        """
+        return 1 + len(self.rest)
+
+    def __getitem__(self, i):
+        """Returning the element found at index i.
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> s[1]
+        2
+        >>> s[2]
+        3
+        """
+        if i == 0:
+            return self.first
+        else:
+            return self.rest[i-1]
+
+    def __setitem__(self, index, element):
+        """Sets the value at the given index to the element
+
+        >>> s = Link(1, Link(2, Link(3)))
+        >>> s[1] = 5
+        >>> s
+        Link(1, Link(5, Link(3)))
+        >>> s[4] = 5
+        Traceback (most recent call last):
+        ...
+        IndexError
+        """
+        if index == 0:
+            self.first = element
+        elif self.rest is Link.empty:
+            raise IndexError
+        else:
+            self.rest[index - 1] = element
+
+    def __contains__(self, e):
+        return self.first == e or e in self.rest
+
+    def map(self, f):
+        self.first = f(self.first)
+        if self.rest is not Link.empty:
+            self.rest.map(f)
+
+def print_link(link):
+    """Print elements of a linked list link.
+
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> print_link(link)
+    <1 2 3>
+    >>> link1 = Link(1, Link(Link(2), Link(3)))
+    >>> print_link(link1)
+    <1 <2> 3>
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> print_link(link1)
+    <3 <4> 5 6>
+    """
+    print('<' + helper(link).rstrip() + '>')
+
+def helper(link):
+    if link == Link.empty:
+        return ''
+    elif isinstance(link.first, Link):
+        return '<' + helper(link.first).rstrip() + '> ' + helper(link.rest)
+    else:
+        return str(link.first) +' '+  helper(link.rest)
+
+# Tree Class
+class Tree:
+    def __init__(self, root, branches=[]):
+        for c in branches:
+            assert isinstance(c, Tree)
+        self.root = root
+        self.branches = branches
+
+    def __repr__(self):
+        if self.branches:
+            branches_str = ', ' + repr(self.branches)
+        else:
+            branches_str = ''
+        return 'Tree({0}{1})'.format(self.root, branches_str)
+
+    def is_leaf(self):
+        return not self.branches
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the entry.
+
+    >>> print_tree(Tree(1))
+    1
+    >>> print_tree(Tree(1, [Tree(2)]))
+    1
+      2
+    >>> numbers = Tree(1, [Tree(2), Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(t.root))
+    for b in t.branches:
+        print_tree(b, indent + 1)
---
+++ lab/lab07/lab07_extra.py
@@ -0,0 +1,73 @@
+from lab07 import *
+
+# Q6
+def reverse_other(t):
+    """Reverse the roots of every other level of the tree using mutation.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(5, [Tree(7), Tree(8)]), Tree(6)]), Tree(3)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(3, [Tree(5, [Tree(8), Tree(7)]), Tree(6)]), Tree(2)])
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q7
+def cumulative_sum(t):
+    """Mutates t where each node's root becomes the sum of all entries in the
+    corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_sum(t)
+    >>> t
+    Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q8
+def deep_map_mut(fn, link):
+    """Mutates a deep link by replacing each item found with the
+    result of calling fn on the item.  Does NOT create new Links (so
+    no use of Link's constructor)
+
+    Does not return the modified Link object.
+
+    >>> link1 = Link(3, Link(Link(4), Link(5, Link(6))))
+    >>> deep_map_mut(lambda x: x * x, link1)
+    >>> print_link(link1)
+    <9 <16> 25 36>
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q9
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
