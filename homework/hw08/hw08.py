fa452e25bf18c591cc0f6e637ff686864282103c
Paul Huang <paul_hzq@hotmail.com>
2016-11-15 22:49:21 -0500

    add homework08

---
+++ homework/hw08/hw08.py
@@ -0,0 +1,20 @@
+
+def deep_len(lst):
+    """Returns the deep length of the list.
+
+    >>> deep_len([1, 2, 3])     # normal list
+    3
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> deep_len(x)
+    4
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> deep_len(x)
+    6
+    >>> x = []
+    >>> for i in range(100):
+    ...     x = [x] + [i]       # very deep list
+    ...
+    >>> deep_len(x)
+    100
+    """
+    "*** YOUR CODE HERE ***"
