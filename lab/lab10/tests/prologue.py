702cca5ff9010da16136fde9feaa2aab28d8ca3b
Paul Huang <paul_hzq@hotmail.com>
2016-12-07 17:32:39 -0500

    add lab10

---
+++ lab/lab10/tests/prologue.py
@@ -0,0 +1,155 @@
+test = {
+  'name': 'Prologue',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'answer': 'a73208f54c693e08d6671873ec5c680d',
+          'choices': [
+            'Turns input into tokens',
+            'Tries to beat Superman',
+            'Organizes tokens in a data structure',
+            'Makes sure that there are no parentheses errors'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What does the lexer do?'
+        },
+        {
+          'answer': '5b1b7da5cb56c4ea1c82e9ef2422b59b',
+          'choices': [
+            'Turns input into tokens',
+            'Organizes tokens in a data structure',
+            'Evaluates the input',
+            'Print the result of evaluation'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What does the parser do?'
+        },
+        {
+          'answer': '8006978b69e088213a68a29f96d158db',
+          'choices': [
+            'Read-Eval-Print-Loop',
+            'Really-Enormous-Purple-Llamas',
+            'Read-Eval-Parse-Lex'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What does REPL stand for?'
+        },
+        {
+          'answer': '65b6edc43cb88f4725318719eada899a',
+          'choices': [
+            'literal, name, call expression, lambda expression',
+            'number, lambda function, primitive function, string',
+            'value, expression, function, number',
+            'name, function, number, literal'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What are all the types of expressions in PyCombinator?'
+        },
+        {
+          'answer': 'edeb45ad4b7bd1fda9d5ab340c9cd779',
+          'choices': [
+            'number, lambda function, primitive function',
+            'number, string, function',
+            'name, number, lambda function',
+            'number, lambda expression, primitive function'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What are all the types of values in PyCombinator?'
+        },
+        {
+          'answer': '9487a632d76c000e0cd9acf25dba6527',
+          'choices': [
+            'a Number',
+            'a String',
+            'a Function',
+            'an Expression'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What does a Literal evaluate to?'
+        },
+        {
+          'answer': '72505e10219d03d5c484574d2c172b9a',
+          'choices': [
+            'They are the same thing',
+            'A lambda expression is the result of evaluating a lambda function',
+            'A lambda function is the result of evaluating a lambda expression',
+            'A lambda expression is a call to a lambda function'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'What is the difference between a lambda expression and a lambda function?'
+        },
+        {
+          'answer': '1558e266fcf3c144eade49469854a739',
+          'choices': [
+            'A method of Expr objects that evaluates the Expr and returns a Value',
+            'A method of Expr objects that evaluates a call expression and returns a Number',
+            'A method of LambdaExpression objects that evaluates a function call',
+            'A method of Literal objects that returns a Name'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'Which of the following describes the eval method?'
+        },
+        {
+          'answer': '052a36e24d69e66f6bacbde1c3c38171',
+          'choices': [
+            'As dictionaries that map variable names (strings) to Value objects',
+            'As sequences of Frame objects',
+            'As dictionaries that map Name objects to Value objects',
+            'As linked lists containing dictionaries'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': 'How are environments represented in our interpreter?'
+        },
+        {
+          'answer': 'c8719397b67eeba8c4826370e9fcc052',
+          'choices': [
+            'Literal(1)',
+            'Number(1)',
+            "Name('1')",
+            'Name(1)'
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': "What will read('1') output?"
+        },
+        {
+          'answer': 'fd0cb816ca4bc1c16a1b18d7c9899909',
+          'choices': [
+            'Literal(x)',
+            'x',
+            'Name(x)',
+            "Name('x')"
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': "What will read('x') output?"
+        },
+        {
+          'answer': '9964a5ee1d418d42c046ba51a5d20093',
+          'choices': [
+            "CallExpr(Literal('add'), Literal(3), Literal(4))",
+            "CallExpr('add', [Literal(3), Literal(4)])",
+            "CallExpr(Name('add'), Literal(3), Literal(4))",
+            "CallExpr(Name('add'), [Literal(3), Literal(4)])"
+          ],
+          'hidden': False,
+          'locked': True,
+          'question': "What will read('add(3, 4)') output?"
+        }
+      ],
+      'scored': False,
+      'type': 'concept'
+    }
+  ]
+}
