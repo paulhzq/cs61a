28095d20faca38c459edcc56248812bd475c36cb
Paul Huang <paul_hzq@hotmail.com>
2016-12-24 10:55:32 -0600

    the homework09 finished

--- homework/hw09/tests/nodots.py
+++ homework/hw09/tests/nodots.py
@@ -7,47 +7,42 @@ test = {
         {
           'code': r"""
           scm> (nodots '(1 . 2))
-          16e7f90f3997618367c64ff4a1c02218
-          # locked
+          (1 2)
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (nodots '(1 2 . 3))
-          48b7a4131742f3894e7fa335b573b4fa
-          # locked
+          (1 2 3)
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (nodots '((1 . 2) 3))
-          a18f22de846ec3d9a784505fd84b4a81
-          # locked
+          ((1 2) 3)
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (nodots '(1 (2 3 . 4) . 3))
-          539ce7c99587b5a410f10ca8bd7e3fab
-          # locked
+          (1 (2 3 4) 3)
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         },
         {
           'code': r"""
           scm> (nodots '(1 . ((2 3 . 4) . 3)))
-          539ce7c99587b5a410f10ca8bd7e3fab
-          # locked
+          (1 (2 3 4) 3)
           """,
           'hidden': False,
-          'locked': True
+          'locked': False
         }
       ],
       'scored': True,
