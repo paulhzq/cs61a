eee6be3ca48937bb9821e3ddabeb797ff5caa137
Paul Huang <paul_hzq@hotmail.com>
2016-12-28 19:12:42 -0600

    finish project scheme part1

--- projects/scheme/scheme_reader.py
+++ projects/scheme/scheme_reader.py
@@ -110,11 +110,11 @@ def scheme_read(src):
     val = src.remove_front() # Get the first token
     if val == 'nil':
         # BEGIN PROBLEM 1
-        "*** REPLACE THIS LINE ***"
+        return nil
         # END PROBLEM 1
     elif val == '(':
         # BEGIN PROBLEM 1
-        "*** REPLACE THIS LINE ***"
+        return read_tail(src)
         # END PROBLEM 1
     elif val == "'":
         # BEGIN PROBLEM 7B
@@ -140,15 +140,23 @@ def read_tail(src):
             raise SyntaxError('unexpected end of file')
         elif src.current() == ')':
             # BEGIN PROBLEM 1
-            "*** REPLACE THIS LINE ***"
+            src.remove_front()
+            return nil
             # END PROBLEM 1
         elif src.current() == '.':
             # BEGIN PROBLEM 2
-            "*** REPLACE THIS LINE ***"
+            src.remove_front()
+            cur = scheme_read(src)
+            if src.remove_front() == ')':
+                return cur
+            else:
+                raise SyntaxError('unexpected .')
             # END PROBLEM 2
         else:
             # BEGIN PROBLEM 1
-            "*** REPLACE THIS LINE ***"
+            first = scheme_read(src)
+            rest = read_tail(src)
+            return Pair(first, rest)
             # END PROBLEM 1
     except EOFError:
         raise SyntaxError('unexpected end of file')
