0a8e15b610683b51d19286d1a36c22333afc7dec
Paul Huang <paul_hzq@hotmail.com>
2017-01-09 23:28:54 -0500

    finish the scheme project

--- projects/scheme/scheme.py
+++ projects/scheme/scheme.py
@@ -532,7 +532,7 @@ def scheme_optimized_eval(expr, env, tail=False):
 ################################################################
 # Uncomment the following line to apply tail call optimization #
 ################################################################
-# scheme_eval = scheme_optimized_eval
+scheme_eval = scheme_optimized_eval
 
 
 ################
