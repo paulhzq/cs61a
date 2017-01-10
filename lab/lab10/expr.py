445b9bf89acffed89dcccc0ce2799bc994abc352
Paul Huang <paul_hzq@hotmail.com>
2017-01-10 00:39:50 -0500

    finish the lab10

--- lab/lab10/expr.py
+++ lab/lab10/expr.py
@@ -110,7 +110,10 @@ class Name(Expr):
         ...     print('Exception raised!')
         Exception raised!
         """
-        "*** YOUR CODE HERE ***"
+        if self.string in env:
+            return env[self.string]
+        else:
+            raise NameError("name '{}' is not defined".format(self.string))
 
     def __str__(self):
         return self.string
@@ -176,7 +179,9 @@ class CallExpr(Expr):
         >>> read('add(mul(3, 4), b)').eval(new_env)
         Number(14)
         """
-        "*** YOUR CODE HERE ***"
+        function = self.operator.eval(env)
+        arguments = [operand.eval(env) for operand in self.operands]
+        return function.apply(arguments)
 
     def __str__(self):
         function = str(self.operator)
@@ -281,7 +286,12 @@ class LambdaFunction(Value):
         if len(self.parameters) != len(arguments):
             raise TypeError("Cannot match parameters {} to arguments {}".format(
                 comma_separated(self.parameters), comma_separated(arguments)))
-        "*** YOUR CODE HERE ***"
+        env = self.parent.copy()
+        #for parameter, argument in zip(self.parameters, arguments):
+        #env[parameter] = argument
+        for i in range(len(self.parameters)):
+            env[self.parameters[i]]=arguments[i]
+        return self.body.eval(env)
 
     def __str__(self):
         definition = LambdaExpr(self.parameters, self.body)
