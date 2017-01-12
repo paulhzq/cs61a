6ac18f05c93d6356ecfb2102bcd9356ed77bdf2f
Paul Huang <paul_hzq@hotmail.com>
2017-01-12 00:18:47 -0500

    finish the hw12

--- homework/hw12/hw12.scm
+++ homework/hw12/hw12.scm
@@ -1,14 +1,31 @@
 (define (find s predicate)
-  'YOUR-CODE-HERE
+  (cond ((null? s) #f)
+  		((predicate (car s)) (car s))
+  		(else (find (cdr-stream s) predicate))
+  )
 )
 
 (define (scale-stream s k)
-  'YOUR-CODE-HERE
+  (cons-stream (* (car s) k) (scale-stream (cdr-stream s) k))
 )
 
 (define (has-cycle s)
-  'YOUR-CODE-HERE
+	(define (tracker s t)
+		(cond
+			((null? t) #f)
+			((eq? s t) #t)
+			(else (tracker s (cdr-stream t)))
+		)
+	)
+	(tracker s (cdr-stream s))
 )
 (define (has-cycle-constant s)
-  'YOUR-CODE-HERE
+  (define (tracker s t)
+		(cond
+			((null? t) #f)
+			((eq? s t) #t)
+			(else (tracker s (cdr-stream t)))
+		)
+	)
+	(tracker s (cdr-stream s))
 )
